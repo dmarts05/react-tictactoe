97b11cebda2ac2f35e23ffd6e9f752347a506e7a
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-02-08 21:33:50 +0100

    Add theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import StartPage from './components/StartPage/StartPage';
+import ThemeToggle from './components/UI/ThemeToggle';
+
 export default function App() {
-  return <div>Start</div>;
+  return (
+    <div className='grid place-content-center'>
+      <StartPage />
+      <ThemeToggle className='fixed bottom-0 right-0 m-4' />
+    </div>
+  );
 }
---
+++ src/components/UI/ThemeToggle.jsx
@@ -0,0 +1,33 @@
+import { animate, motion } from 'framer-motion';
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
+
+export default function ThemeToggle(props) {
+  const [isDarkModeActive, setIsDarkModeActive] = useState(false);
+
+  const toggleThemeHandler = () => {
+    setIsDarkModeActive(prev => !prev);
+    console.log(document.getRootNode());
+    document.documentElement.classList.toggle('dark');
+  };
+
+  return (
+    <motion.button
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      transition={{ duration: 0.2 }}
+      onClick={toggleThemeHandler}
+      className={`w-16 rounded-full bg-black p-4 text-white dark:bg-white dark:text-black ${props.className}`}
+    >
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        exit={{ scale: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        {!isDarkModeActive && <MoonIcon />}
+        {isDarkModeActive && <SunIcon />}
+      </motion.div>
+    </motion.button>
+  );
+}
