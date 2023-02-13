41573eef3a7ccb16417f0a739892a5be4688f292
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-02-13 22:20:09 +0100

    Fix h-screen not working on mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import StartPage from './components/StartPage/StartPage';
 
 export default function App() {
   return (
-    <div className='grid place-content-center'>
+    <div className='absolute inset-0 grid place-content-center'>
       <StartPage />
     </div>
   );
