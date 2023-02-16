c59ba0757f0d3a4cd03f33c89a12f77d09ce3647
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-02-16 23:47:40 +0100

    Prevent user from clicking when it is AI's turn

--- src/components/GamePage/Cell.jsx
+++ src/components/GamePage/Cell.jsx
@@ -5,7 +5,7 @@ export default function Cell({ index, content }) {
   const board = useGameStore(state => state.board);
   const currentTurn = useGameStore(state => state.currentTurn);
   const isEndGame = useGameStore(state => state.isEndGame);
-  // const gamePreferences = useGameStore(state => state.gamePreferences);
+  const gamePreferences = useGameStore(state => state.gamePreferences);
   const setBoard = useGameStore(state => state.setBoard);
   const setCurrentTurn = useGameStore(state => state.setCurrentTurn);
   const setIsEndGame = useGameStore(state => state.setIsEndGame);
@@ -14,6 +14,18 @@ export default function Cell({ index, content }) {
     // Check if cell has already been used
     if (content !== ' ') {
       return;
+    }
+    // Check if it is AI's turn
+    else if (gamePreferences.gameMode === 'ai' && currentTurn === 'X') {
+      return;
+    }
+    // Check if the game has ended
+    else {
+      for (const key in isEndGame) {
+        if (isEndGame[key]) {
+          return;
+        }
+      }
     }
 
     const newBoard = [...board];
@@ -25,14 +37,7 @@ export default function Cell({ index, content }) {
 
   return (
     <div
-      onClick={() => {
-        for (const key in isEndGame) {
-          if (isEndGame[key]) {
-            return;
-          }
-        }
-        performMoveHandler();
-      }}
+      onClick={performMoveHandler}
       className='grid h-24 w-24 cursor-pointer select-none place-content-center rounded-2xl border-4 border-white text-5xl font-medium'
     >
       {content}
