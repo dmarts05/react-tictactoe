5fdfe5ccbfcd7c768dfe67b3ee1b938159f9ef7e
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-02-17 21:02:29 +0100

    Add minimax algorithm for impossible mode AI

--- src/logic/get-ai-move-index.js
+++ src/logic/get-ai-move-index.js
@@ -1,6 +1,96 @@
-const getBestMoveIndex = extendedInfoBoard => {
-  // const bestValue = Number.MIN_SAFE_INTEGER;
-  const bestMoveIndex = -1;
+import getIsEndGame from './get-is-end-game';
+
+let extendedInfoBoard;
+const MAX_DEPTH = 6;
+
+const getHeuristicValue = () => {
+  const isEndGame = getIsEndGame(extendedInfoBoard.map(cell => cell.content));
+
+  return isEndGame.hasPlayerOneWon ? -10 : isEndGame.hasPlayerTwoWon ? 10 : 0;
+};
+
+const minimax = (depth, isMaximizing, alfa, beta) => {
+  const isEndGame = getIsEndGame(extendedInfoBoard.map(cell => cell.content));
+  if (
+    depth === MAX_DEPTH ||
+    isEndGame.hasPlayerOneWon ||
+    isEndGame.hasPlayerTwoWon ||
+    isEndGame.isTie
+  ) {
+    return getHeuristicValue();
+  }
+
+  const availableCells = extendedInfoBoard.filter(cell => cell.content === ' ');
+
+  let bestValue;
+  if (isMaximizing) {
+    // Maximize
+    bestValue = Number.MIN_SAFE_INTEGER;
+
+    const BreakError = {};
+
+    try {
+      availableCells.forEach(cell => {
+        // Temporary place AI piece in the board
+        extendedInfoBoard[cell.index] = { ...cell, content: 'X' };
+
+        // Get heuristic value for this movement
+        bestValue = Math.max(
+          bestValue,
+          minimax(depth + 1, !isMaximizing, alfa, beta)
+        );
+
+        // Update alfa value
+        alfa = Math.max(alfa, bestValue);
+
+        // Remove temporary placed AI piece from the board
+        extendedInfoBoard[cell.index] = { ...cell, content: ' ' };
+
+        if (beta <= alfa) {
+          throw BreakError;
+        }
+      });
+    } catch (error) {
+      if (error !== BreakError) throw error;
+    }
+  } else {
+    // Minimize
+    bestValue = Number.MAX_SAFE_INTEGER;
+
+    const BreakError = {};
+
+    try {
+      availableCells.forEach(cell => {
+        // Temporary place adversary piece in the board
+        extendedInfoBoard[cell.index] = { ...cell, content: 'O' };
+
+        // Get heuristic value for this movement
+        bestValue = Math.min(
+          bestValue,
+          minimax(depth + 1, !isMaximizing, alfa, beta)
+        );
+
+        // Update beta value
+        beta = Math.min(beta, bestValue);
+
+        // Remove temporary placed AI piece from the board
+        extendedInfoBoard[cell.index] = { ...cell, content: ' ' };
+
+        if (beta <= alfa) {
+          throw BreakError;
+        }
+      });
+    } catch (error) {
+      if (error !== BreakError) throw error;
+    }
+  }
+
+  return bestValue;
+};
+
+const getBestMoveIndex = () => {
+  let bestValue = Number.MIN_SAFE_INTEGER;
+  let bestMoveIndex = -1;
 
   const availableCells = extendedInfoBoard.filter(cell => cell.content === ' ');
 
@@ -8,13 +98,28 @@ const getBestMoveIndex = extendedInfoBoard => {
     // Temporary place AI piece in the board
     extendedInfoBoard[cell.index] = { ...cell, content: 'X' };
 
-    // Keep going...
+    // Get heuristic value for this movement
+    const value = minimax(
+      1,
+      false,
+      Number.MIN_SAFE_INTEGER,
+      Number.MAX_SAFE_INTEGER
+    );
+
+    // Remove temporary placed AI piece from the board
+    extendedInfoBoard[cell.index] = { ...cell, content: ' ' };
+
+    // Check if we have found a better move
+    if (value > bestValue) {
+      bestValue = value;
+      bestMoveIndex = cell.index;
+    }
   });
 
   return bestMoveIndex;
 };
 
-const getRandomMoveIndex = extendedInfoBoard => {
+const getRandomMoveIndex = () => {
   const availableCells = extendedInfoBoard.filter(cell => cell.content === ' ');
 
   return availableCells[Math.floor(Math.random() * availableCells.length)]
@@ -22,7 +127,7 @@ const getRandomMoveIndex = extendedInfoBoard => {
 };
 
 const getAiMoveIndex = (board, difficulty) => {
-  const extendedInfoBoard = board.map((cell, index) => ({
+  extendedInfoBoard = board.map((cell, index) => ({
     content: cell,
     index,
   }));
@@ -30,7 +135,7 @@ const getAiMoveIndex = (board, difficulty) => {
   switch (difficulty) {
     case 'easy':
       return getRandomMoveIndex(extendedInfoBoard);
-    case 'normal':
+    case 'impossible':
       return getBestMoveIndex(extendedInfoBoard);
     default:
       return getRandomMoveIndex(extendedInfoBoard);
